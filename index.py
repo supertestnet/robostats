b30798385502c5b3cf8e569087e77afb0d0f6d8c
supertestnet <58400631+supertestnet@users.noreply.github.com>
2026-03-08 02:52:08 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 # requires two dependencies:
 # sudo apt install python3-flask python3-socks python3-requests
-# or pip3 install flask socks requests
+# or pip3 install flask socks requests[socks]
 
 import requests
 from flask import Flask, request, url_for, make_response, send_file
